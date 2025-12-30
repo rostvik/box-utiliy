5ac9a266208b20f4c60ce9393038aa3708119337
Roxedus <me@roxedus.dev>
2025-12-30 22:07:26 +0000

    feat(ci/cd): Add Ansible prehook

---
+++ .pre-commit/docsible.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# arr=()
+
+
+source .venv/bin/activate
+
+for file in $(git diff --cached --name-only); do
+    changed=$(echo $file | awk -F '/' '{print $1 "/" $2}')
+    dir_changed+="-r $changed\n"
+done
+
+dir_changed=$(echo -en $dir_changed | uniq)
+
+docsible --comments --append --graph --no-backup --no-docsible $dir_changed
